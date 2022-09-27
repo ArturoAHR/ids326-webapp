6910f4006e97b2c7aa9bf6f6acaaf731c87c399f
RukShu <arturoaureliohr@gmail.com>
2022-09-26 22:17:24 -0400

    Remove dotEnv initialization

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
-import * as dotenv from 'dotenv';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-
-dotenv.config();
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
