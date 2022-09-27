ebfafe6f73f2845696f0935f2825d612fec645a2
RukShu <arturoaureliohr@gmail.com>
2022-09-26 21:04:05 -0400

    Format ESLint config file

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,38 +1,38 @@
 // eslint-disable-next-line no-undef
 module.exports = {
-    'env': {
-        'browser': true,
-        'es2021': true,
-        'jest': true // Add this line!
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'plugin:@typescript-eslint/recommended',
-        'prettier'
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint', 'react-hooks', 'prettier'],
+  rules: {
+    'spaced-comment': 'error',
+    quotes: ['error', 'single'],
+    'no-duplicate-imports': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
     ],
-    'overrides': [
-    ],
-    'parser': '@typescript-eslint/parser',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
     },
-    'plugins': [
-        'react',
-        '@typescript-eslint',
-        'react-hooks',
-        'prettier'
-    ],
-    'rules': {
-        'spaced-comment': 'error',
-        'quotes': ['error', 'single'],
-        'no-duplicate-imports': 'error',
-        'react/react-in-jsx-scope': 'off',
-    },
-    'settings': {
-        'import/resolver': {
-          'typescript': {}
-        }
-    }
-}
+  },
+};
