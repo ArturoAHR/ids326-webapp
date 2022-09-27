69742f7486eb1657fc3f9e048341b2eb4b184aac
RukShu <arturoaureliohr@gmail.com>
2022-09-27 19:28:39 -0400

    Add Tabs MUI component to CRUD Menu

--- src/pages/CRUD/CRUD.tsx
+++ src/pages/CRUD/CRUD.tsx
@@ -1,3 +1,19 @@
-export const CRUD = () => {
-  return <>CRUD</>;
+import { Tab, Tabs } from '@mui/material';
+import { FC, ReactNode } from 'react';
+
+type CRUDProps = {
+  children?: ReactNode;
+};
+
+export const CRUD: FC<CRUDProps> = ({ children }) => {
+  return (
+    <div>
+      <Tabs>
+        <Tab label="Item One" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
+      </Tabs>
+      {children}
+    </div>
+  );
 };
