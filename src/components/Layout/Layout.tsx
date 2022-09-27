23a8fa44b3d7608afe80134b98074fed0659e373
RukShu <arturoaureliohr@gmail.com>
2022-09-27 00:19:56 -0400

    Implement Layout styling

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,12 +1,14 @@
 import { FC, ReactNode } from 'react';
 
+import './Layout.css';
+
 type LayoutProps = {
   children: ReactNode;
 };
 
 export const Layout: FC<LayoutProps> = ({ children }) => {
   return (
-    <div>
+    <div className="layout-container">
       <div className="layout-sidebar"></div>
       <div className="layout-content">{children}</div>
     </div>
