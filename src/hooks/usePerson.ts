abc2b530df62249e36ba9b8f25ee1298bb0c2d72
RukShu <arturoaureliohr@gmail.com>
2022-09-28 00:37:28 -0400

    Add deletePerson method to usePerson hook

--- src/hooks/usePerson.ts
+++ src/hooks/usePerson.ts
@@ -1,22 +1,29 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { PersonMapper } from '../mappers/person';
 import { PersonService } from '../services/person-service';
-import { Person } from '../types/person';
+import { DeletePersonRequest, Person } from '../types/person';
 
 export const usePerson = () => {
   const [people, setPeople] = useState<Person[]>([]);
 
+  const personService: PersonService = useMemo(() => new PersonService(), []);
+
   const fetchPeople = async () => {
-    const personService: PersonService = new PersonService();
     const newPeople: Person[] = PersonMapper.mapTransformToEntityType(
       await personService.getAll(),
     );
     setPeople(newPeople);
   };
 
+  const deletePerson = async (dto: DeletePersonRequest) => {
+    const deleted: boolean = await personService.delete(dto);
+    await fetchPeople();
+    return deleted;
+  };
+
   useEffect(() => {
     fetchPeople();
   }, []);
 
-  return { people, fetchPeople };
+  return { people, fetchPeople, deletePerson };
 };
