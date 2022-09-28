005a6019ffcb77324241193e5e07dfc8c612247b
RukShu <arturoaureliohr@gmail.com>
2022-09-27 22:48:07 -0400

    Add Company types

---
+++ src/types/company.ts
@@ -0,0 +1,20 @@
+import { BaseEntityType } from './base-entity-type';
+import { Person } from './person';
+
+export type BaseCompany = {
+  name: string;
+  ceoId: string;
+  primaryPhone: string;
+  secondaryPhone: string;
+  fax: string;
+  email: string;
+  website: string;
+  ceo?: Person;
+};
+
+export type GetCompanyResponse = BaseEntityType &
+  BaseCompany & {
+    __ceo__?: Person;
+  };
+
+export type Company = BaseEntityType & BaseCompany;
