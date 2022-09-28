efff94f7932d1714a98fb3566ea15223ece44203
RukShu <arturoaureliohr@gmail.com>
2022-09-27 22:22:02 -0400

    Add GetPeopleResponse type (WIP)

--- src/types/person.ts
+++ src/types/person.ts
@@ -11,4 +11,15 @@ export type Person = BaseEntityType & {
   contactTypeId: string;
   departmentId: string;
   companyId: string;
+  company: any;
+  role: any;
+  department: any;
+  contactType: any;
+};
+
+export type GetPeopleResponse = Person & {
+  __company__: any;
+  __role__: any;
+  __department__: any;
+  __contactType__: any;
 };
