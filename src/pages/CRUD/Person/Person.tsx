0f3e19ca0a8d1402883b7358439392f6a6b6be19
RukShu <arturoaureliohr@gmail.com>
2022-09-27 22:09:06 -0400

    Add DataGrid

--- src/pages/CRUD/Person/Person.tsx
+++ src/pages/CRUD/Person/Person.tsx
@@ -1,3 +1,26 @@
-export const Person = () => {
-  return <>Person</>;
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
+import { Person } from '../../../types/person';
+
+export const PersonCRUD = () => {
+  const columns: GridColDef[] = [
+    {
+      field: 'firstName',
+      headerName: 'First name',
+      width: 150,
+      editable: true,
+    },
+    {
+      field: 'lastName',
+      headerName: 'Last name',
+      width: 150,
+      editable: true,
+    },
+  ];
+
+  const rows: Person[] = [];
+  return (
+    <div className="crud-person-container">
+      <DataGrid columns={columns} rows={rows} />
+    </div>
+  );
 };
