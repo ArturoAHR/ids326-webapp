1bc70c5c2039e0a2d38a9dd40ad5e6ec18ea5fcf
RukShu <arturoaureliohr@gmail.com>
2022-09-27 21:13:12 -0400

    Add handleChange method to change to the corresponding window when selecting a tab

--- src/pages/CRUD/CRUD.tsx
+++ src/pages/CRUD/CRUD.tsx
@@ -1,17 +1,31 @@
 import { Tab, Tabs } from '@mui/material';
-import { FC, ReactNode } from 'react';
+import { FC, ReactNode, SyntheticEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 type CRUDProps = {
   children?: ReactNode;
 };
 
+enum CRUDTabMenuOptions {
+  Person = 'person',
+  Role = 'role',
+  ContactType = 'contact-type',
+}
+
 export const CRUD: FC<CRUDProps> = ({ children }) => {
+  const navigate = useNavigate();
+
+  const handleChange = (event: SyntheticEvent, value: CRUDTabMenuOptions) => {
+    event.preventDefault();
+    navigate('/hw-1/crud/' + value);
+  };
+
   return (
     <div>
-      <Tabs>
-        <Tab label="Item One" />
-        <Tab label="Item Two" />
-        <Tab label="Item Three" />
+      <Tabs onChange={handleChange}>
+        <Tab label="Person" value={CRUDTabMenuOptions.Person} />
+        <Tab label="Role" value={CRUDTabMenuOptions.Role} />
+        <Tab label="Contact Type" value={CRUDTabMenuOptions.ContactType} />
       </Tabs>
       {children}
     </div>
