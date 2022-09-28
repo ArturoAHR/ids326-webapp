574f859a8e3cac0a871005f6199fc002c28f2856
RukShu <arturoaureliohr@gmail.com>
2022-09-28 00:06:44 -0400

    Change Tabs component implementation to non-deprecated one

--- src/pages/CRUD/CRUD.tsx
+++ src/pages/CRUD/CRUD.tsx
@@ -29,11 +29,15 @@ export const CRUD: FC<CRUDProps> = ({ children }) => {
   return (
     <div className="crud-menu-container">
       <h1 className="crud-menu-title">CRUD</h1>
-      <Tabs defaultActiveKey={optionSelected} onChange={handleChange}>
-        <Tabs.TabPane tab="Person" key={CRUDTabMenuOptions.Person} />
-        <Tabs.TabPane tab="Role" key={CRUDTabMenuOptions.Role} />
-        <Tabs.TabPane tab="Contact Type" key={CRUDTabMenuOptions.ContactType} />
-      </Tabs>
+      <Tabs
+        defaultActiveKey={optionSelected}
+        onChange={handleChange}
+        items={[
+          { label: 'Person', key: CRUDTabMenuOptions.Person },
+          { label: 'Role', key: CRUDTabMenuOptions.Role },
+          { label: 'Contact Type', key: CRUDTabMenuOptions.ContactType },
+        ]}
+      />
       {children}
     </div>
   );
