eec163c829602783c25a6bfd8be878b1e9560a26
RukShu <arturoaureliohr@gmail.com>
2022-09-27 22:42:16 -0400

    Refactor Base Entity types

--- src/types/base-entity-type.ts
+++ src/types/base-entity-type.ts
@@ -1,6 +1,11 @@
-export type BaseEntityType = {
+export type BaseIdType = {
   id: string;
+};
+
+export type BaseCrudType = {
   createdAt: Date;
   updatedAt: Date;
   deletedAt?: Date;
 };
+
+export type BaseEntityType = BaseIdType & BaseCrudType;
