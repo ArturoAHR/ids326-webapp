10f0993a4f5d870ca5b961bfcead196a27b20f84
RukShu <arturoaureliohr@gmail.com>
2022-09-27 22:33:33 -0400

    Fix getAll PersonService method

--- src/services/person-service.ts
+++ src/services/person-service.ts
@@ -1,10 +1,13 @@
+import { GetPeopleResponse } from '../types/person';
 import { AxiosService } from './axios-service';
 
 export class PersonService extends AxiosService {
   private readonly URL = '/person';
 
-  getAll = async () => {
-    const { data: results, status } = await this.axios.get(this.URL);
-    if ((status === 200 || status === 201) && results.data) return results.data;
+  getAll = async (): Promise<GetPeopleResponse[]> => {
+    const { data: results, status } = await this.axios.get<GetPeopleResponse[]>(
+      this.URL,
+    );
+    return results;
   };
 }
