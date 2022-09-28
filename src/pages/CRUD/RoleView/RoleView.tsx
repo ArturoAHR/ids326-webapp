63f74180aa812f36b9c2478c0f94c7397bd9d13b
RukShu <arturoaureliohr@gmail.com>
2022-09-28 19:46:36 -0400

    Add basic component structure of Role View

--- src/pages/CRUD/RoleView/RoleView.tsx
+++ src/pages/CRUD/RoleView/RoleView.tsx
@@ -1,3 +1,47 @@
-export const RoleView = () => {
-  return <>RoleView</>;
+import { Button, Col, Row, Table } from 'antd';
+import { FC, useState } from 'react';
+import { useRole } from '../../../hooks/useRole';
+import { Role } from '../../../types/role';
+
+export const RoleView: FC = () => {
+  const { roles } = useRole();
+
+  const [selectedRole, setSelectedRole] = useState<Role>();
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+    },
+  ];
+
+  return (
+    <div className="crud-role-container">
+      <Row className="crud-person-button-group" gutter={16} justify={'end'}>
+        <Col>Button</Col>
+        <Col>Button</Col>
+        <Col>
+          <Button disabled={!selectedRole}>Delete</Button>
+        </Col>
+      </Row>
+      <div className="crud-person-table">
+        <Table
+          columns={columns}
+          dataSource={roles}
+          scroll={{ x: 'max-content', y: 400 }}
+          rowKey={(role) => role.id}
+          rowSelection={{
+            type: 'radio',
+            onSelect: (role) => {
+              setSelectedRole(role);
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 };
