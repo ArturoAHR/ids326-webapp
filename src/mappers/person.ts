b4f449f7834d853ffa8edf1d24eb0cadb649436c
RukShu <arturoaureliohr@gmail.com>
2022-09-27 23:09:20 -0400

    Implement Foreign Entity mappers in Person Mapper

--- src/mappers/person.ts
+++ src/mappers/person.ts
@@ -1,13 +1,22 @@
 import { GetPeopleResponse, Person } from '../types/person';
+import { CompanyMapper } from './company';
+import { ContactTypeMapper } from './contact-type';
+import { DepartmentMapper } from './department';
+import { RoleMapper } from './role';
 
 export class PersonMapper {
   static transformToEntityType = (dto: GetPeopleResponse): Person => {
     const person = {
       ...dto,
-      company: dto.__company__,
-      role: dto.__role__,
-      department: dto.__department__,
-      contactType: dto.__contactType__,
+      company:
+        dto.__company__ && CompanyMapper.transformToEntityType(dto.__company__),
+      role: dto.__role__ && RoleMapper.transformToEntityType(dto.__role__),
+      department:
+        dto.__department__ &&
+        DepartmentMapper.transformToEntityType(dto.__department__),
+      contactType:
+        dto.__contactType__ &&
+        ContactTypeMapper.transformToEntityType(dto.__contactType__),
     };
     return person;
   };
