bb74d52d83cb258c9eab15d69354db8bce291c15
RukShu <arturoaureliohr@gmail.com>
2022-09-27 23:02:10 -0400

    Rename variables in Person Mapper methods to improve readability

--- src/mappers/person.ts
+++ src/mappers/person.ts
@@ -2,20 +2,20 @@ import { GetPeopleResponse, Person } from '../types/person';
 
 export class PersonMapper {
   static transformToEntityType = (dto: GetPeopleResponse): Person => {
-    const newPerson = {
+    const person = {
       ...dto,
       company: dto.__company__,
       role: dto.__role__,
       department: dto.__department__,
       contactType: dto.__contactType__,
     };
-    return newPerson;
+    return person;
   };
 
   static mapTransformToEntityType = (dto: GetPeopleResponse[]): Person[] => {
-    const newPeople = dto.map((personDto) =>
+    const people = dto.map((personDto) =>
       this.transformToEntityType(personDto),
     );
-    return newPeople;
+    return people;
   };
 }
