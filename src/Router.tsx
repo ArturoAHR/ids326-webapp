c02393ed0f14d11835c14cfdc6977464d89e0a61
RukShu <arturoaureliohr@gmail.com>
2022-09-27 21:27:25 -0400

    Add Three CRUD routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,11 +1,39 @@
 import { Route, Routes } from 'react-router-dom';
+import { ContactType } from './pages/CRUD/ContactType/ContactType';
 import { CRUD } from './pages/CRUD/CRUD';
+import { Person } from './pages/CRUD/Person/Person';
+import { Role } from './pages/CRUD/Role/Role';
 
 export const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<></>}></Route>
-      <Route path="/hw-1/crud" element={<CRUD />} />
+      <Route path="/">
+        <></>
+      </Route>
+      <Route
+        path="/hw-1/crud/person"
+        element={
+          <CRUD>
+            <Person />
+          </CRUD>
+        }
+      />
+      <Route
+        path="/hw-1/crud/role"
+        element={
+          <CRUD>
+            <Role />
+          </CRUD>
+        }
+      />
+      <Route
+        path="/hw-1/crud/contact-type"
+        element={
+          <CRUD>
+            <ContactType />
+          </CRUD>
+        }
+      />
     </Routes>
   );
 };
