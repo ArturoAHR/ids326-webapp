e7641fbd455d165173a01f5844c46d3212d6788e
RukShu <arturoaureliohr@gmail.com>
2022-10-09 15:22:08 -0400

    Add DepartmentView component structure

--- src/pages/CRUD/DepartmentView/DepartmentView.tsx
+++ src/pages/CRUD/DepartmentView/DepartmentView.tsx
@@ -1,5 +1,65 @@
-import { FC } from 'react';
+import { Row, Col, Button, Table } from 'antd';
+import { FC, useState } from 'react';
+import { useDepartment } from '../../../hooks/useDepartment';
+import { Department } from '../../../types/department';
 
 export const DepartmentView: FC = () => {
-  return <></>;
+  const { departments, fetchDepartments, deleteDepartment } = useDepartment();
+
+  const [selectedDepartment, setSelectedDepartment] = useState<Department>();
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      width: 250,
+    },
+    {
+      title: 'Code',
+      dataIndex: 'code',
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+    },
+  ];
+
+  const handleDelete = () => {
+    deleteDepartment({ id: selectedDepartment!.id });
+  };
+
+  return (
+    <div className="crud-department-container">
+      <Row className="crud-department-button-group" gutter={16} justify={'end'}>
+        <Col>{/* <CreateEditDepartment refetch={fetchDepartments} /> */}</Col>
+        <Col>
+          {/* <CreateEditDepartment
+            editMode
+            disabled={!selectedDepartment}
+            selectedDepartment={selectedDepartment}
+            refetch={fetchDepartments}
+          /> */}
+        </Col>
+        <Col>
+          <Button disabled={!selectedDepartment} onClick={handleDelete}>
+            Delete
+          </Button>
+        </Col>
+      </Row>
+      <div className="crud-department-table">
+        <Table
+          columns={columns}
+          dataSource={departments}
+          scroll={{ x: 'max-content', y: 400 }}
+          rowKey={(department) => department.id}
+          rowSelection={{
+            type: 'radio',
+            onSelect: (department) => {
+              setSelectedDepartment(department);
+            },
+          }}
+        />
+      </div>
+    </div>
+  );
 };
